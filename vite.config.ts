fc8115928936b0a9eb4e93f8b92e663d9f0e32f4
Dansora375 <danisora375@gmail.com>
2026-02-16 23:49:57 -0500

    Update base path in Vite configuration to match portfolio deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-deploy/",
+  base: "/Portafolio/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
